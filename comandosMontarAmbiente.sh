4b89c7ebe630876852d9cc2aad91050b5e01e5df
edixred <edixred@hotmail.com>
2022-02-25 12:20:58 -0500

    Update comandosMontarAmbiente.sh

--- comandosMontarAmbiente.sh
+++ comandosMontarAmbiente.sh
@@ -2,7 +2,10 @@
 #desplegar imagen Docker
 docker-compose -f Docker/Jenkins.yml up -d
 # obtener contraseña
+echo ______________LOG-JENKINS______________
 docker logs jenkins 
+echo ______________COPEANDO BACKUP______________
 cp Docker/AllBackup jenkins/
+echo ______________FIN______________
 
 
