92a2a8794b0e21abe596f901d2cb3413d51d2477
edixred <edixred@hotmail.com>
2022-02-27 21:20:56 -0500

    Update comandosMontarAmbiente.sh

--- comandosMontarAmbiente.sh
+++ comandosMontarAmbiente.sh
@@ -1,4 +1,3 @@
-
 #desplegar imagen Docker
 docker-compose -f Docker/Jenkins.yml up -d
 # obtener contraseña
@@ -6,6 +5,9 @@ echo ______________LOG-JENKINS______________
 docker logs jenkins 
 echo ______________COPEANDO BACKUP______________
 cp Docker/AllBackup jenkins/
-echo ______________FIN______________
+echo ______________DESCARGANDO BACKUP PLUGINS______________
+scp -i Docker/demokey ubuntu@ec2-34-205-146-122.compute-1.amazonaws.com:/home/ubuntu/bkjenkins.tar .
+tar -xvf bkjenkins.tar
+docker restart jenkins
 
 
