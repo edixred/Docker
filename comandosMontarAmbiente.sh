8bb15e073141acdc67a89f665327f6747e4c9845
edixred <edixred@hotmail.com>
2022-02-27 22:05:46 -0500

    Update comandosMontarAmbiente.sh

--- comandosMontarAmbiente.sh
+++ comandosMontarAmbiente.sh
@@ -6,9 +6,9 @@ echo ______________ restaurando BACKUP Jobs ______________
 cp Docker/AllBackup jenkins/
 echo ______________ Descargando BACKUP Plugins ______________
 chmod 400 Docker/demokey
-scp -i Docker/demokey ubuntu@ec2-34-205-146-122.compute-1.amazonaws.com:/home/ubuntu/bkjenkins2.tar .
+scp -i Docker/demokey ubuntu@ec2-34-205-146-122.compute-1.amazonaws.com:/home/ubuntu/bkjenkins.tar .
 echo ______________ Restaurando BACKUP Plugins ______________
-tar -xvf bkjenkins2.tar
+tar -xvf bkjenkins.tar
 docker restart jenkins
 sleep 20
 echo ______________ Reinicio Terminado ______________
